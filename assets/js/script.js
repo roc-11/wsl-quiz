ac6637a1b03a0e3c9eb247803fe08f721f7d5148
roc-11 <>
2023-09-20 11:00:04 +0000

    Style navigation menu for responsive design and add user functionality to the burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,13 @@ const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
 navToggle.addEventListener('click', () => {
-    
-})
+    const isVisible = primaryNav.getAttribute('data-visible');
+
+    if (isVisible === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (isVisible === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
