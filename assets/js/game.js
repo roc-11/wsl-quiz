96e79a031f9dc8b41f0e17d13c921dd890fc3992
roc-11 <>
2023-09-22 07:29:34 +0000

    Add code to display visual feedback for correct/incorrect answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -104,8 +104,18 @@ choices.forEach((choice) => {
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
 
-        //once answer, get next question
-        getNewQuestion();
+        //apply correct / incorrect class to animate user selection
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        selectedChoice.parentElement.classList.add(classToApply);
+     
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            //once answer, get next question
+            getNewQuestion();
+        }, 1000);
+
     });
 });
 
