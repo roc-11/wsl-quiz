c35d0b07fdcd46fd5afb1c17cd3420f29e1d6da2
roc-11 <>
2023-09-22 09:32:46 +0000

    Fix: fix missing id for username in end.html

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,6 +1,6 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('final-score');
+const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
@@ -9,6 +9,7 @@ finalScore.innerText = mostRecentScore;
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
+
 
 saveHighScore = (e) => {
     //prevent posting to a different page
