590cf7e7f2ddeb5cc0591aac8ac777903abe2719
roc-11 <>
2023-09-22 09:42:56 +0000

    Add code to save scores to local storage

--- assets/js/end.js
+++ assets/js/end.js
@@ -2,6 +2,11 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+//convert array to JSON (everything in local storage is a string)
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 //disable save button if username value empty / falsy
@@ -14,4 +19,15 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     //prevent posting to a different page
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    //add new score to the highscore array
+    highScores.push(score);
+
+    //sort list, check highest, save top 5
+    highScores.sort((a, b) => return b.score - a.score);
+    highScores.splice(5);
 };
