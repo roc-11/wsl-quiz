7bec56e88f46eff2964d1469ff099886180ea80b
roc-11 <>
2023-09-29 16:25:21 +0000

    Add code to display message to user when no high scores exist

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -10,20 +10,28 @@ if (highScores.length === 0) {
     //alert('no scores yet');
 
     //NEED TO SORT THIS FOR NEW TABLE !!!
-    var para = document.createElement("p");
-    var node = document.createTextNode("No high scores yet!");
-    para.appendChild(node);
-    highScoreContainer.appendChild(para);
+    let noScoreRowOne = highScoresList.insertRow(0);
+    console.log(noScoreRowOne);
+    let cell1 = noScoreRowOne.insertCell(0);
+    cell1.classList.add("noHighScores");
+    let newText = document.createTextNode("No scores yet! Click play to get on the leaderboard!");
+    // cell1.innerHTML('No scores yet!');
+    cell1.appendChild(newText);
+    console.log(cell1);
     //highScoreContainer.innerHTML = `<p>No high scores yet! <br>Click PLAY to try to get on the leaderboard!</p>`;
 } else {
 
     //iterate through highScores and add <li> for each
     highScoresList.innerHTML = highScores.map(score => {
         //return `<li class="high-score">${score.name}: ${score.score}</li>`;
-        return `<tr class="high-score">
+        return `<tr class="high-score hover">
                     <td class='number'><img class="trophy-svg" src="assets/images/trophy-svg.svg"></td>
                     <td class='name'>${score.name}</td>
                     <td class='point'>${score.score}</td> 
                     </tr>`;
     }).join("");
+
+    /*let firstRow = highScoresList.rows[0];
+    firstRow.classList.remove("hover");
+    firstRow.classList.add("firstRowHover"); */
 }
